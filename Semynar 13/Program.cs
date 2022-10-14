94149bf915eefccb7105f639f1c6f3eafb60fc42
Kinder <kindermaks@mail.ru>
2022-10-14 11:22:40 +0300

    semynar

---
+++ Semynar 13/Program.cs
@@ -0,0 +1,52 @@
+﻿
+/*
+Console.Write("input a number: "); // Запрос информации, 
+int num = Convert.ToInt32(Console.ReadLine());
+
+num = num * 2;
+
+Console.WriteLine("Your number is " + num);
+*/
+
+/*
+Console.Write("input a first number: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+int quad2 = n2 * n2;
+
+if (n1 == quad2) // если в вветвлении 1 команда то {} ставить не обязательно, если больше то скобки строго нужны
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
+*/
+
+// Задача 2. 
+/*
+Console.WriteLine("Intut positive integer a number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int current = -n;
+
+while (current <= n)
+{
+    Console.Write(current + " ");
+    current++; // увеличивает на единицу ( +=2 если нужно увеличить на 2)
+}
+*/
+
+// Задача 3
+
+/*
+Console.Write("Input a three-digit number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int result = num % 10;
+Console.WriteLine($"Lust digit of {num} is {result}");
+
+*/
